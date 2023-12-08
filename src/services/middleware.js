42e5d1cb68bdca3967638b4a84cd4bf1d22cc6a4
Dynastiy <emmanuel00michael@gmail.com>
2023-12-08 20:55:00 +0100

    updated invest

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -1,11 +1,19 @@
 import $request from "@/https/middleware";
 
 const middleware = {
-  // Swap
-  async swap(formData) {
+ 
+
+   // stake
+   async stake(formData) {
+    return $request.post("stake", formData).then((response) => response.data)
+  },
+
+   // Swap
+   async swap(formData) {
     return $request.post("swap", formData).then((response) => response.data)
   },
 
+  // Swap Rate
   async getStakeRate(payload) {
     return $request.get(`conversion-rate?from=${payload.from}&to=${payload.to}`).then((response) => response.data)
   }
